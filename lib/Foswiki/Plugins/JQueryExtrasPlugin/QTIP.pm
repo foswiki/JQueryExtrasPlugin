01c8f830602838784daf37738299aa2142c03041
PaulHarvey <PaulHarvey@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-03-04 07:42:42 +0000

    Item8665: Create a plugin to hold jquery javascript plugins that shouldn't be shipped with core JQueryPlugin
    git-svn-id: http://svn.foswiki.org/trunk/JQueryExtrasPlugin@6644 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ lib/Foswiki/Plugins/JQueryExtrasPlugin/QTIP.pm
@@ -0,0 +1,57 @@
+# Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+# 
+# Javascript is Copyright (C) 2009 Craig Thompson, craig@craigsworks.com
+#
+# Packaged 2010/03 for Foswiki by Paul.W.Harvey@csiro.au - www.taxonomy.org.au
+# Centre for Plant Biodiversity Research CSIRO Plant Industry- www.csiro.au/pi
+# 
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later 
+# version. 
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details,
+# published at http://www.gnu.org/copyleft/gpl.html
+
+package Foswiki::Plugins::JQueryExtrasPlugin::QTIP;
+use strict;
+use warnings;
+use base 'Foswiki::Plugins::JQueryPlugin::Plugin';
+
+=begin TML
+
+---+ package Foswiki::Plugins::JQueryExtrasPlugin::QTIP
+
+This is the perl stub for the jquery.qtip plugin.
+
+=cut
+
+=begin TML
+
+---++ ClassMethod new( $class, $session, ... )
+
+Constructor
+
+=cut
+
+sub new {
+  my $class = shift;
+  my $session = shift || $Foswiki::Plugins::SESSION;
+
+  my $this = bless($class->SUPER::new( 
+    $session,
+    name => 'qTip',
+    version => '1.0.0-rc3',
+    author => 'Craig Thompson',
+    homepage => 'http://craigsworks.com/projects/qtip/',
+    documentation => "$Foswiki::cfg{SystemWebName}.JQueryQTip",
+    puburl => '%PUBURLPATH%/%SYSTEMWEB%/JQueryExtrasPlugin/qtip',
+    javascript => ['jquery.qtip.js']
+  ), $class);
+
+  return $this;
+}
+
+1;
