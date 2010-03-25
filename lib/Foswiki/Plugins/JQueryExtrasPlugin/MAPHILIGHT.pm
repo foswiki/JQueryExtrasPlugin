5f685dbcca631269bbc6f7e7370e1a280cfab403
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-03-25 14:52:31 +0000

    Item8765: Replace use base with use; our @ISA; pass 2
    git-svn-id: http://svn.foswiki.org/trunk/JQueryExtrasPlugin@6913 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/JQueryExtrasPlugin/MAPHILIGHT.pm
+++ lib/Foswiki/Plugins/JQueryExtrasPlugin/MAPHILIGHT.pm
@@ -18,7 +18,8 @@
 package Foswiki::Plugins::JQueryExtrasPlugin::MAPHILIGHT;
 use strict;
 use warnings;
-use base 'Foswiki::Plugins::JQueryPlugin::Plugin';
+use Foswiki::Plugins::JQueryPlugin::Plugin;
+our @ISA = qw( Foswiki::Plugins::JQueryPlugin::Plugin );
 
 =begin TML
 
